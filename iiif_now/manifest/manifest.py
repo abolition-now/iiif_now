8566dd0c10947561e7d6638333b5e8886306e6b5
Mark Baggett <mbagget1@utk.edu>
2024-03-31 19:31:52 -0400

    Add initial code and todos.

--- iiif_now/manifest/manifest.py
+++ iiif_now/manifest/manifest.py
@@ -2,7 +2,10 @@ from iiif_prezi3 import Manifest, config, KeyValueString, load_bundled_extension
 import requests
 import json
 
+
 class ANManifest:
+    # @Todo: Add navPlace
+    # @Todo: Add Video Canvases
     def __init__(self, manifest_data, extensions=[]):
         self.config = config.configs['helpers.auto_fields.AutoLang'].auto_lang = "en"
         self.manifest_data = manifest_data
@@ -20,22 +23,27 @@ class ANManifest:
         )
         for canvas in self.manifest_data['canvases']:
             if canvas['type'] == 'Image':
-                canvas = manifest.make_canvas_from_iiif(
-                    url=self.metadata['image_api'],
-                    id=f"https://aboltion-now-manifests.s3.us-east-2.amazonaws.com/{canvas['key']}/canvas/{canvas['sequence']}",
-                    anno_id=f"https://aboltion-now-manifests.s3.us-east-2.amazonaws.com/{canvas['key']}/canvas/{canvas['sequence']}/annotation/1",
-                    anno_page_id=f"https://aboltion-now-manifests.s3.us-east-2.amazonaws.com/{canvas['key']}/canvas/{canvas['sequence']}/annotation/1/page/1",
-                )
+                try:
+                    # @Todo: Break Bucket Identifiers Out of Code
+                    canvas = manifest.make_canvas_from_iiif(
+                        url=f"https://strob6zro3bzklrulaqu2545sy0odbvz.lambda-url.us-east-2.on.aws/iiif/3/{canvas['key']}",
+                        id=f"https://aboltion-now-manifests.s3.us-east-2.amazonaws.com/{canvas['key']}/canvas/{canvas['sequence']}",
+                        anno_id=f"https://aboltion-now-manifests.s3.us-east-2.amazonaws.com/{canvas['key']}/canvas/{canvas['sequence']}/annotation/1",
+                        anno_page_id=f"https://aboltion-now-manifests.s3.us-east-2.amazonaws.com/{canvas['key']}/canvas/{canvas['sequence']}/annotation/1/page/1",
+                    )
+                # @Todo: Accurately Define Exception
+                except:
+                    print(f'Missing {canvas["key"]} in bucket.')
         x = manifest.json(indent=2)
         y = json.loads(x)
         y['@context'] = ["http://iiif.io/api/extension/navplace/context.json", "http://iiif.io/api/presentation/3/context.json"]
+        # @Todo: Write Manifests in Separate Method
         # with open(f'data/new_copy/{self.metadata["key"]}.json', 'w') as outfile:
         #     outfile.write(json.dumps(y, indent=2))
         return manifest.json(indent=2)
 
     def __build_metadata(self):
         metadata = []
-        print(self.manifest_data['metadata'])
         for k, v in self.manifest_data['metadata'].items():
             metadata.append(
                 KeyValueString(
@@ -47,7 +55,9 @@ class ANManifest:
 
 
 if __name__ == "__main__":
+    # Note: This is all temporary until parent package is complete.
     from iiif_now.csvreader import DataReader
+    # @Todo: All of this should happen elsewhere. Bring in configuration first then fix.
     reader = DataReader(
         'data/march302024.csv',
         artists_file='data/artists_codes.csv',

5c874b7524ece5f9407a717b151483375f4e877a
Mark Baggett <mbagget1@utk.edu>
2024-03-31 11:25:14 -0400

    Add other canvas data values.

--- iiif_now/datacanvas/datacanvas.py
+++ iiif_now/datacanvas/datacanvas.py
@@ -7,6 +7,11 @@ class DataCanvas:
         self.metadata_data = metadata_data
         self.artists = self.__find_canvas_artists()
         self.metadata = self.__build_metadata()
+        self.label = self.canvas_data['canvas title']
+        self.sequence = self.canvas_data['sequence']
+        self.parent = self.canvas_data['parent']
+        self.type = self.canvas_data['type']
+        self.thumbnail = self.__find_thumbnail_source()
 
     def __find_canvas_artists(self):
         artists = []
@@ -23,3 +28,9 @@ class DataCanvas:
                 field = self.metadata_data[v]
                 metadata.setdefault(field, []).append(v)
         return metadata
+
+    def __find_thumbnail_source(self):
+        if self.canvas_data['thumbnail source'] != '':
+            return self.canvas_data['thumbnail source']
+        else:
+            return self.canvas_data['key']
